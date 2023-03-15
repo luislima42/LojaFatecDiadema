5022c6a7bebee01ce524609b636a0bd4505a30c5
Vinicius Heltai <heltai@usp.br>
2023-03-15 10:22:00 -0300

    Versão1.0 - Estrutura das rotas do projeto

--- src/main/java/com/fatec/loja/controller/HomeController.java
+++ src/main/java/com/fatec/loja/controller/HomeController.java
@@ -1,4 +1,31 @@
 package com.fatec.loja.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
 public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/perfumaria")
+    public String perfumaria(){
+        return "perfumaria";
+    }
+
+    @GetMapping("/roupa")
+    public String roupa(){
+        return "roupa";
+    }
+
+    @GetMapping("/suplemento")
+    public String suplemento(){
+        return "suplemento";
+    }
 }
+
